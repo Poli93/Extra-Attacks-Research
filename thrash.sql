b83fcaba9fb37b4352972b6b233fc3503276cd47
Idan <tjyeee@gmail.com>
2025-01-13 14:08:11 +0200

    Create thrash.sql

---
+++ thrash.sql
@@ -0,0 +1,44 @@
+-- mobs with Thrash cased by proc chance & PPM
+SELECT
+    abc.name, 
+    abc.id, 
+    abc.auras,
+    CASE 
+        WHEN abc.auras = 3417 THEN 10
+        WHEN abc.auras = 8876 THEN 15
+        WHEN abc.auras = 12787 THEN 35
+        WHEN abc.auras = 3391 THEN "?"
+        ELSE NULL
+    END AS proc_chance,
+    CASE 
+        WHEN abc.auras = 3417 THEN ((10 / 100) * 60) / 4
+        WHEN abc.auras = 8876 THEN ((15 / 100) * 60) / 4
+        WHEN abc.auras = 12787 THEN ((35 / 100) * 60) / 4
+        WHEN abc.auras = 3391 THEN "?"
+        ELSE NULL
+    END AS ppm
+FROM (
+    SELECT DISTINCT 
+        b.name, 
+        a.caster_id AS id, 
+        a.spell_id AS auras
+    FROM 
+        spell_unique_caster a
+        JOIN creature_template_wdb b 
+        ON a.caster_id = b.entry
+    WHERE 
+        a.spell_id IN (3391,3417,8876,12787)
+    
+    UNION
+    
+    SELECT DISTINCT 
+        b.name, 
+        a.id AS id, 
+        a.auras AS auras
+    FROM 
+        creature a
+        JOIN creature_template_wdb b 
+        ON a.id = b.entry
+    WHERE 
+        a.auras IN (3391,3417,8876,12787)
+) abc;
