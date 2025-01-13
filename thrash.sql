437551dd932470379e871bf7bfde2eb8e9a77269
Idan <tjyeee@gmail.com>
2025-01-13 15:26:28 +0200

    Update thrash.sql

--- thrash.sql
+++ thrash.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT
     sp.id, 
     sp.auras, 
     cre.level,
-    map.MapName_Lang_enUS,
+    map.MapName_Lang_enUS as zone,
     CASE 
         WHEN sp.auras = 3417 THEN 10
         WHEN sp.auras = 8876 THEN 15
@@ -46,4 +46,4 @@ FROM (
 ) sp, creature cre, db_Map_12340 map
 WHERE sp.id = cre.id
 AND cre.map = map.id
-ORDER BY proc_chance DESC
+ORDER BY proc_chance, sp.name DESC
